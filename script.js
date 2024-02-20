b013082131ca2e3acc60de7c6d8c67db8b139a95
rajaisrajan <126311145+rajaisrajan@users.noreply.github.com>
2024-02-20 20:31:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 // Add your JavaScript animations using GSAP here
 gsap.from("h1", { opacity: 0, duration: 1, y: -50, ease: "power4.out" });
 gsap.from("nav", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 0.5 });
