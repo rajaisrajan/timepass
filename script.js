4ee32044547d56be3d1870318ad68bce2528389a
rajaisrajan <126311145+rajaisrajan@users.noreply.github.com>
2024-02-20 20:09:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Add your JavaScript animations using GSAP here
+gsap.from("h1", { opacity: 0, duration: 1, y: -50, ease: "power4.out" });
+gsap.from("nav", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 0.5 });
+gsap.from("#home", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 1 });
+gsap.from("#menu", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 1.5 });
+gsap.from("#about", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 2 });
+gsap.from("#contact", { opacity: 0, duration: 1, y: -50, ease: "power4.out", delay: 2.5 });
